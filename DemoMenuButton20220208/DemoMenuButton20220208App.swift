9150df99d4e12f24de22f48611a3bd51b1a1d727
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-08 15:40:34 -0500

    Initial Commit

---
+++ DemoMenuButton20220208/DemoMenuButton20220208App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoMenuButton20220208App.swift
+//  DemoMenuButton20220208
+//
+//  Created by Xavier on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoMenuButton20220208App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
